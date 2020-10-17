cf0b7642dded8aa2e46bd56b96e11613ea4af0e7
Zero-zzzz <1205730144@qq.com>
2020-10-17 15:02:10 +0800

    SSM+MyBatisPlus集成完成

---
+++ crm-service/src/test/java/cn/javasm/test/DemoTest.java
@@ -0,0 +1,20 @@
+package cn.javasm.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = {"classpath*:applicationContext-mapper.xml","classpath*:applicationContext-service.xml"})
+public class DemoTest {
+
+
+
+    @Test
+    public void doTest(){
+
+    }
+
+}
